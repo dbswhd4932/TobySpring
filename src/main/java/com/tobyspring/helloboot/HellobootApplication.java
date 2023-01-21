6e5c6215d6f858e80d7a272782067238e5a142af
yoonjong <dbswhd4932@gmail.com>
2023-01-21 16:14:28 +0900

    빈 서블릿 컨테이너 만들기

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,18 @@
 package com.tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class HellobootApplication {
 
+    // 빈 서블릿 컨테이너 만들기 ( 내장형 톰캣 라이브러리가 이미 존재 )
     public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
-    }
+//        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
 
+    }
 }
