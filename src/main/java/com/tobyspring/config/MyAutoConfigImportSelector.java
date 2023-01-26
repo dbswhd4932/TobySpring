05403f8c5f5e5b4bb771081ab751540fb2de75c8
yoonjong <dbswhd4932@gmail.com>
2023-01-26 12:45:57 +0900

    자동 구성 정보 등록

---
+++ src/main/java/com/tobyspring/config/MyAutoConfigImportSelector.java
@@ -0,0 +1,20 @@
+package com.tobyspring.config;
+
+import org.springframework.boot.context.annotation.ImportCandidates;
+import org.springframework.context.annotation.DeferredImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    @Override
+    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+        ImportCandidates candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+
+    }
+}
